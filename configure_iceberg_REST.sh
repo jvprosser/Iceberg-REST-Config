b03eba77497fa27634cc1c278cf6aab5a27f579e
Joe Prosser <jvprosser@gmail.com>
2025-08-12 12:31:56 -0400

    Update configure_iceberg_REST.sh
    added REST calls to confim registry access

--- configure_iceberg_REST.sh
+++ configure_iceberg_REST.sh
@@ -183,9 +183,12 @@ curl -k -u "${cdpadmin_user}:${cdpadmin_passwd}" -H "Accept: application/json" -
 
 set +o xtrace
 
-echo "display Database"
+echo "display Database, tables and table details"
 
 AT=$(curl -k -X POST -H  "Content-Type: application/x-www-form-urlencoded" -d "client_id=${CLIENT_ID}&client_secret=${SECRET}&grant_type=client_credentials" "https://${KNOX_HOST}/${DL_NAME}/cdp-share-access/hms-api/icecli/v1/oauth/tokens" | jq -r '.access_token')
 
 curl -ivk -X GET -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Bearer ${AT}" https://${KNOX_HOST}/${DL_NAME}/cdp-share-access/hms-api/icecli/v1/namespaces
+
+curl -ivk -X GET -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Bearer ${AT}" https://${KNOX_HOST}/${DL_NAME}/cdp-share-access/hms-api/icecli/v1/namespaces/${DATABASE}/tables
+curl -ivk -X GET -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Bearer ${AT}" https://${KNOX_HOST}/${DL_NAME}/cdp-share-access/hms-api/icecli/v1/namespaces/${DATABASE}/tables/${ICEBERG_TABLE}
 echo "DONE"
